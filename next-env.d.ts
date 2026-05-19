247d52e0520c488ae0770839638a884f880159ab
SoulSparkler <soulsparklers@gmail.com>
2026-05-19 11:50:39 +0200

    Force Vercel clean rebuild - clear cache for new blog post
    Triggering fresh build to include business-starter-kits-women-entrepreneurs-2026
    in generateStaticParams()

--- next-env.d.ts
+++ next-env.d.ts
@@ -4,3 +4,4 @@ import "./.next/types/routes.d.ts";
 
 // NOTE: This file should not be edited
 // see https://nextjs.org/docs/app/api-reference/config/typescript for more information.
+// Force Vercel clean build - Tue May 19 11:26:53 CEST 2026
